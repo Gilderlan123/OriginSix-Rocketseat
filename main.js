e0d2a43a09a854360fc76decaa08d4021a43a157
Gilderlan Silva <35428534+Gilderlan123@users.noreply.github.com>
2022-03-20 00:25:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+/* Abre e Fecha o Menu ao clicar nas 3 setas */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+/* Mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+
+}
+
+
+/* Testimonials Carrossel*/
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
+})
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links 
+   `, { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+
+
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+
+
+/*====== Menu Ativo conforme seção visível*/
+const sections = document.querySelectorAll('main section[id')
+
+function activateMenuAtCurrentSection() {
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document.querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+
+    }
+
+
+}
+
+
+
+
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
